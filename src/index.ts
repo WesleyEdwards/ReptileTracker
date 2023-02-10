b6c3d046c376e3f6656e78ad78586b100bf17d01
medwards <maxedwards20@gmail.com>
2023-02-10 08:52:40 -0700

    Updated the TODO items and rearranged index.ts to match the assignemtn descritpion stories

--- src/index.ts
+++ src/index.ts
@@ -22,25 +22,27 @@ app.use(express.json()); // middleware to convert everything to json
 
 // TODO:
 // Post user signs in
+// Make sure the user can only access reptiles that are associated with their account
+// Make sure the user can't access anything unless signed in
 
 app.post("/users", createUser);
 app.get("/users", getAllUsers);
 
 app.post("/reptiles", createReptile);
-app.get("/reptiles", getReptiles);
-app.put("/reptiles/:id", updateReptile);
 app.delete("/reptiles/:id", deleteReptile);
-
-app.post("/schedules", createSchedule);
-app.get("/schedules", getAllSchedules);
-app.get("/schedules/:id", getScheduleByReptile);
-app.get("/schedules/user/:id", getScheduleByUser);
+app.put("/reptiles/:id", updateReptile);
+app.get("/reptiles", getReptiles);
 
 app.post("/feeding/", createFeeding);
 app.get("/feeding/:id", getAllFeedings); // id indicates the reptile id we want to get all feedings for
 
 app.post("/record/", createHusbandryRecord);
 app.get("/record/:id", getAllRecords);
+
+app.post("/schedules", createSchedule);
+app.get("/schedules", getAllSchedules);
+app.get("/schedules/:id", getScheduleByReptile);
+app.get("/schedules/user/:id", getScheduleByUser);
 
 app.get("/", (req, res) => {
   res.send(`<h1>Welcome to the homepage</h1>`);
